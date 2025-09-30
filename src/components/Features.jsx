b4d7a37fe8c6b5960419ba8849f8abae365de1a1
prcollective <175355791+prcollective@users.noreply.github.com>
2025-09-30 13:44:33 -0400

    updates

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -126,7 +126,7 @@ const Features = () => (
       <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
         <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
           <BentoCard
-            src="videos/aboutvideo.mp4"
+            src="videos/about4.mp4"
             title={
               <>
                 HOTTEST MUSIC SOUNDTRACK
